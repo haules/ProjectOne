a6154e6cac483e8516b41c0b56ce9665619d11d8
Alex I <alexxsmen1986@gmail.com>
2023-07-25 11:13:39 +0100

    final

---
+++ library-app-master/src/Components/MenuBar.java
@@ -0,0 +1,51 @@
+package Components;
+
+import Departments.BorrowBooks;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+
+public class MenuBar {
+
+    private static JMenuBar menuBar;
+
+    private static JMenu fileMenu;
+    private static JMenu helpMenu;
+
+    private static JMenuItem saveItem;
+    private static JMenuItem exitItem;
+
+    private static JMenuItem version;
+
+
+    public static JMenuBar getMenuBar() {
+        menuBar = new JMenuBar();
+        menuBar.setBackground(Color.BLUE);
+
+        fileMenu = new JMenu("File");
+        helpMenu = new JMenu("Help");
+
+        saveItem = new JMenuItem("Save");
+        exitItem = new JMenuItem("Exit");
+        version = new JMenuItem("Version");
+
+        fileMenu.add(saveItem);
+        fileMenu.add(exitItem);
+
+        helpMenu.add(version);
+
+        menuBar.add(fileMenu);
+        menuBar.add(helpMenu);
+
+        setExitItem(exitItem);
+        return menuBar;
+    }
+
+
+    public static void setExitItem(JMenuItem exit) {
+        exit.addActionListener(e -> {
+            System.exit(1);
+        });
+    }
+}
---
+++ library-app-master/src/DatabaseConnection/DBConnection.java
@@ -0,0 +1,25 @@
+package DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static Connection connection;
+    private static String JDBC_Driver = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/mydb";
+
+    public static void connect() {
+        try {
+            Class.forName(JDBC_Driver);
+            connection = DriverManager.getConnection(DATABASE_URL, "root", "root");
+            System.out.println("Successfully connected");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+}
